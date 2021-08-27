11ef24c543c299688d0532d10ab0089900755342
SteveTheDragon <draconis36@gmail.com>
2021-08-26 18:43:02 -0700

    Finished adding animations and animator tree

---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour
+{
+
+    public Animator anim;
+    public int currentAnim = 0;
+    
+    public void Next()
+    {
+        
+        if (currentAnim == 2)
+        {
+            //do nothing
+        }
+        else
+        {
+            currentAnim ++;
+        }
+            
+        anim.SetInteger("NextAnim", currentAnim);
+    }
+
+    public void Previous()
+    {
+        
+        if (currentAnim == 0)
+        {
+            //do nothing 
+        }
+        else
+        {
+            currentAnim --;
+        }           
+
+        anim.SetInteger("NextAnim", currentAnim);    
+
+    }
+}
