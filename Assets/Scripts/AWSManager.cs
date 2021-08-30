a2f91a4eea948d368a0a73554f8f2cdd7184d73b
SteveTheDragon <draconis36@gmail.com>
2021-08-30 12:07:07 -0700

    Started from scratch and AWSManager is workinggit add .!

--- Assets/Scripts/AWSManager.cs
+++ Assets/Scripts/AWSManager.cs
@@ -11,78 +11,82 @@ using Amazon.Runtime;
 
 public class AWSManager : MonoBehaviour
 {
-    private static AWSManager _instance;
-    public static AWSManager Instance
-    {
-        get
-        {
-            if (_instance == null)
+   
+  private static AWSManager _instance;
+  public static AWSManager Instance
+  {
+      get
+      {
+          if(_instance == null)
+          {
+              Debug.LogError("AWSManager is null!");
+          }
+          return _instance;
+      }
+  }
+
+  void Awake()
+  {
+      _instance = this;
+      UnityInitializer.AttachToGameObject(this.gameObject);
+
+      //initialize http client
+      AWSConfigs.HttpClient = AWSConfigs.HttpClientOption.UnityWebRequest;
+  }
+
+  public string S3Region = RegionEndpoint.USWest1.SystemName;
+  private RegionEndpoint _S3Region
+  {
+      get
+      {
+          return RegionEndpoint.GetBySystemName(S3Region);
+      }
+  }
+
+  private AmazonS3Client _s3Client;
+  public AmazonS3Client S3Client
+  {
+      get
+      {
+          if(_s3Client==null)
+          {
+              _s3Client = new AmazonS3Client(
+                  new CognitoAWSCredentials(
+                      Keys.AMAZON_KEY, RegionEndpoint.USWest1)
+                      , _S3Region);
+          }
+          else
             {
-                Debug.LogError("AWSManager is null!");
+                Debug.Log("s3Client:"+_s3Client);
             }
-            return _instance;
-        }
-    }
+          return _s3Client;
+      }
+   }
 
-    public string S3Region = RegionEndpoint.USWest1.SystemName;
-    private RegionEndpoint _S3Region
-    {
-        get{return RegionEndpoint.GetBySystemName(S3Region);}
-    }   
-
-    private AmazonS3Client _s3Client;
-    public AmazonS3Client S3Client
-    {
-        get
-        {
-            if(_s3Client == null)
-            {
-                _s3Client = new AmazonS3Client(
-                    new CognitoAWSCredentials(Keys.AMAZON_KEY, //Identity pool ID
-                    RegionEndpoint.USWest1 //Region
-                    ), _S3Region);
-            }
-
-            return _s3Client;
-        }
-    }
-
-    void Awake()
-    {
-         
-        _instance = this;
-        UnityInitializer.AttachToGameObject(this.gameObject);
-        //initialize the http client
-        AWSConfigs.HttpClient = AWSConfigs.HttpClientOption.UnityWebRequest;       
-
-    }
-
-    void Start()
-    {
-        Debug.Log("S3Client: " + S3Client);
-        
-        //Create a constructor
+   void Start()
+   {
         var request = new ListObjectsRequest()
         {
             BucketName = "sdaranatomy"
         };
 
-        Debug.Log(request);
-
-        S3Client.ListObjectsAsync(request, (responseObject) =>
+        S3Client.ListObjectsAsync(request, (responseObj) =>
         {
-            if (responseObject.Exception == null)
+            if(responseObj.Exception == null)
             {
-                responseObject.Response.S3Objects.ForEach((obj) => 
+                responseObj.Response.S3Objects.ForEach((obj) =>
                 {
                     Debug.Log("Object: "+obj.Key);
                 });
             }
             else
             {
-                Debug.Log("Response Object: "+responseObject.Exception);
+                Debug.Log("Exception: "+responseObj.Exception);
             }
         });
-    }
+   }
+
   
+
+ 
 }
